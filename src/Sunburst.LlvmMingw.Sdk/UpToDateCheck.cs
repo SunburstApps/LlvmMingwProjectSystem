caaa10a2264cf2e7a5def4370ac5c0055ee78025
William Kent <wjk011@gmail.com>
2020-08-26 23:12:31 -0400

    Add UpToDateCheck task

---
+++ src/Sunburst.LlvmMingw.Sdk/UpToDateCheck.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Sunburst.LlvmMingw.Sdk
+{
+    public sealed class UpToDateCheck : Task
+    {
+        [Required]
+        public ITaskItem[] InputFiles { get; set; }
+
+        [Required]
+        public ITaskItem[] OutputFiles { get; set; }
+
+        [Output]
+        public ITaskItem[] FilesToRemove { get; set; }
+
+        public override bool Execute()
+        {
+            try
+            {
+                Dictionary<string, DateTime> dateCache = new Dictionary<string, DateTime>();
+
+                foreach (ITaskItem file in OutputFiles)
+                {
+                    FileInfo info = new FileInfo(file.GetMetadata("FullPath"));
+                    dateCache.Add(info.FullName, info.LastWriteTimeUtc);
+                }
+
+                var toRemove = new List<ITaskItem>();
+                foreach (ITaskItem file in InputFiles)
+                {
+                    FileInfo info = new FileInfo(file.GetMetadata("FullPath"));
+                    if (dateCache.Values.All(date => date <= info.LastWriteTimeUtc))
+                    {
+                        toRemove.Add(file);
+                    }
+                }
+
+                FilesToRemove = toRemove.ToArray();
+            }
+            catch (Exception ex)
+            {
+                Log.LogErrorFromException(ex);
+            }
+
+            return !Log.HasLoggedErrors;
+        }
+    }
+}
