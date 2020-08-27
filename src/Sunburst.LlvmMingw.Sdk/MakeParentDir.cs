a2a0580d136c1b217515ea25753bed87540a2c23
William Kent <wjk011@gmail.com>
2020-08-27 19:24:29 -0400

    Add and use MakeParentDir task
    This simplifies the process of creating the directories
    needed to write widl output considerably.

---
+++ src/Sunburst.LlvmMingw.Sdk/MakeParentDir.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Sunburst.LlvmMingw.Sdk
+{
+    public sealed class MakeParentDir : Task
+    {
+        [Required]
+        public ITaskItem[] Files { get; set; }
+
+        [Output]
+        public ITaskItem[] CreatedDirectories { get; set; }
+
+        public override bool Execute()
+        {
+            try
+            {
+                List<ITaskItem> outputs = new List<ITaskItem>();
+
+                foreach (var file in Files)
+                {
+                    string path = Path.GetDirectoryName(file.GetMetadata("FullPath"));
+                    bool existed = Directory.Exists(path);
+                    Directory.CreateDirectory(path);
+                    outputs.Add(new TaskItem(path));
+
+                    if (!existed) Log.LogMessage("Created directory {0}.", path);
+                }
+
+                CreatedDirectories = outputs.ToArray();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Log.LogErrorFromException(ex);
+                return false;
+            }
+        }
+    }
+}
