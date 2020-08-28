d200491080f8deea49a3cf7f303f6008da6fe54a
William Kent <wjk011@gmail.com>
2020-08-28 16:44:01 -0400

    Refactor WidlTaskBase out of WidlGenerateHeaders

--- src/Sunburst.LlvmMingw.Sdk/WidlGenerateHeaders.cs
+++ src/Sunburst.LlvmMingw.Sdk/WidlGenerateHeaders.cs
@@ -5,80 +5,30 @@ using Microsoft.Build.Utilities;
 
 namespace Sunburst.LlvmMingw.Sdk
 {
-    public class WidlGenerateHeaders : ToolTask
+    public class WidlGenerateHeaders : WidlTaskBase
     {
-        [Required]
-        public ITaskItem SourceFile { get; set; }
-
         [Output]
         public ITaskItem[] Headers { get; set; }
-
-        [Output]
-        public ITaskItem[] FileWrites { get; set; }
-
-        [Required]
-        public string LlvmMingwRoot { get; set; }
-
-        [Required]
-        public string TargetArchitecture { get; set; }
 
         [Required]
         public string OutputDirectory { get; set; }
 
-        public ITaskItem[] IncludeDirectories { get; set; }
-        public string[] PreprocessorDefinitions { get; set; }
+        [Required]
         public string OutputHeaderFileName { get; set; }
-        public bool EnableWarnings { get; set; }
 
-        private TaskItem GeneratedHeader = null;
-
-        protected override string ToolName => Utility.ExpandToolName(TargetArchitecture, "widl");
-        protected override string GenerateFullPathToTool()
+        protected override void OnExecuteSuccess()
         {
-            string filename = Utility.ExpandToolName(TargetArchitecture, "widl");
-            return Path.Combine(LlvmMingwRoot, "bin", filename);
+            var generatedHeader = new TaskItem(Path.Combine(OutputDirectory, OutputHeaderFileName));
+            generatedHeader.SetMetadata("UsesPCH", "False");
+
+            Headers = new[] { generatedHeader };
+            FileWrites = new[] { generatedHeader };
         }
 
-        public override bool Execute()
+        protected override void GenerateCommandLineCommandsCore(CommandLineBuilder builder)
         {
-            try
-            {
-                GeneratedHeader = new TaskItem(Path.Combine(OutputDirectory, OutputHeaderFileName));
-                GeneratedHeader.SetMetadata("UsesPCH", "False");
-
-                bool success = base.Execute();
-                if (success)
-                {
-
-                    Headers = new[] { GeneratedHeader };
-                    FileWrites = new[] { GeneratedHeader };
-                }
-
-                return success;
-            }
-            catch (Exception ex) when (ex is InvalidOperationException || ex is ArgumentException)
-            {
-                Log.LogErrorFromException(ex);
-                return false;
-            }
-        }
-
-        protected override string GenerateCommandLineCommands()
-        {
-            CommandLineBuilder builder = new CommandLineBuilder();
-
             builder.AppendSwitch("-h");
-            builder.AppendSwitchIfNotNull("-o ", GeneratedHeader);
-            if (EnableWarnings) builder.AppendSwitch("-W");
-
-            foreach (ITaskItem path in IncludeDirectories)
-            {
-                builder.AppendSwitchIfNotNull("-I ", path.ItemSpec);
-            }
-            foreach (string define in PreprocessorDefinitions)
-            {
-                builder.AppendSwitchIfNotNull("-D ", define);
-            }
+            builder.AppendSwitchIfNotNull("-o ", Path.Combine(OutputDirectory, OutputHeaderFileName));
 
             if (TargetArchitecture == "Win32" || TargetArchitecture == "x86" || TargetArchitecture == "ARM")
             {
@@ -88,9 +38,6 @@ namespace Sunburst.LlvmMingw.Sdk
             {
                 builder.AppendSwitch("--win64");
             }
-
-            builder.AppendFileNameIfNotNull(SourceFile);
-            return builder.ToString();
         }
 
         protected override string GetWorkingDirectory() => OutputDirectory;
---
+++ src/Sunburst.LlvmMingw.Sdk/WidlTaskBase.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Sunburst.LlvmMingw.Sdk
+{
+    public abstract class WidlTaskBase : ToolTask
+    {
+        [Required]
+        public ITaskItem IdlFile { get; set; }
+
+        [Output]
+        public ITaskItem[] FileWrites { get; set; }
+
+        [Required]
+        public string LlvmMingwInstallRoot { get; set; }
+
+        [Required]
+        public string TargetArchitecture { get; set; }
+
+        public ITaskItem[] IncludeDirectories { get; set; }
+        public string[] PreprocessorDefinitions { get; set; }
+        public bool EnableWarnings { get; set; }
+
+        protected sealed override string GenerateCommandLineCommands()
+        {
+            CommandLineBuilder builder = new CommandLineBuilder();
+            if (EnableWarnings) builder.AppendSwitch("-W");
+            foreach (var item in IncludeDirectories) builder.AppendSwitchIfNotNull("-I ", item.ItemSpec);
+            foreach (var item in PreprocessorDefinitions) builder.AppendSwitchIfNotNull("-D ", item);
+
+            GenerateCommandLineCommandsCore(builder);
+
+            builder.AppendFileNameIfNotNull(IdlFile);
+            return builder.ToString();
+        }
+
+        protected abstract void GenerateCommandLineCommandsCore(CommandLineBuilder builder);
+
+        protected virtual void OnExecuteSuccess() { }
+
+        public sealed override bool Execute()
+        {
+            try
+            {
+                bool success = base.Execute();
+                if (success) OnExecuteSuccess();
+                return success;
+            }
+            catch (Exception ex) when (ex is InvalidOperationException || ex is ArgumentException)
+            {
+                Log.LogErrorFromException(ex);
+                return false;
+            }
+        }
+
+        protected sealed override string ToolName => Utility.ExpandToolName(TargetArchitecture, "widl");
+        protected sealed override string GenerateFullPathToTool()
+        {
+            string filename = Utility.ExpandToolName(TargetArchitecture, "widl");
+            return Path.Combine(LlvmMingwInstallRoot, "bin", filename);
+        }
+    }
+}
