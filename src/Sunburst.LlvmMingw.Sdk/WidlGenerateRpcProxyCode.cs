0e83145add82c31d5c47a734d407b7e3600f63b0
William Kent <wjk011@gmail.com>
2020-08-28 21:01:51 -0400

    Add WidlGenerateRpcProxyCode task

---
+++ src/Sunburst.LlvmMingw.Sdk/WidlGenerateRpcProxyCode.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Sunburst.LlvmMingw.Sdk
+{
+    public class WidlGenerateRpcProxyCode : WidlTaskBase
+    {
+        [Required]
+        public ITaskItem OutputFilePath { get; set; }
+
+        [Output]
+        public ITaskItem[] GeneratedSources { get; set; }
+
+        public string StubType { get; set; }
+
+        protected override bool ValidateParameters()
+        {
+            if (!string.IsNullOrEmpty(StubType) && !(StubType == "Os" || StubType == "Oi" || StubType == "Oif"))
+            {
+                Log.LogError("Invailid StubType (expected \"Os\", \"Oi\", or \"Oif\")");
+                return false;
+            }
+
+            return base.ValidateParameters();
+        }
+
+        protected override void GenerateCommandLineCommandsCore(CommandLineBuilder builder)
+        {
+            builder.AppendSwitch("-p");
+            builder.AppendSwitchIfNotNull("-o ", OutputFilePath);
+
+            if (StubType == "Os") builder.AppendSwitch("-Os");
+            else if (StubType == "Oi") builder.AppendSwitch("-Oi");
+            else if (StubType == "Oif") builder.AppendSwitch("-Oif");
+        }
+
+        protected override void OnExecuteSuccess()
+        {
+            var item = new TaskItem(OutputFilePath);
+            GeneratedSources = new[] { item };
+            FileWrites = new[] { item };
+        }
+    }
+}
