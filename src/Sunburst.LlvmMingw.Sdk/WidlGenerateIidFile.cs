bc97832f2038747b4dd11b76767c70d9c2945503
William Kent <wjk011@gmail.com>
2020-08-28 20:57:01 -0400

    Add WidlGenerateIidFile task

---
+++ src/Sunburst.LlvmMingw.Sdk/WidlGenerateIidFile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Sunburst.LlvmMingw.Sdk
+{
+    public class WidlGenerateIidFile : WidlTaskBase
+    {
+        [Required]
+        public ITaskItem OutputFilePath { get; set; }
+
+        [Output]
+        public ITaskItem[] GeneratedSources { get; set; }
+
+        protected override void GenerateCommandLineCommandsCore(CommandLineBuilder builder)
+        {
+            builder.AppendSwitch("-u");
+        }
+
+        protected override void OnExecuteSuccess()
+        {
+            var item = new TaskItem(OutputFilePath);
+            GeneratedSources = new[] { item };
+            FileWrites = new[] { item };
+        }
+    }
+}
