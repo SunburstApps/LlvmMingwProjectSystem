e5e4cb2e20239eefc88d31cb850e5df23b8289b1
William Kent <wjk011@gmail.com>
2020-08-28 20:40:20 -0400

    Add WidlGenerateRpcClientStub.StubType support

--- src/Sunburst.LlvmMingw.Sdk/WidlGenerateRpcClientStub.cs
+++ src/Sunburst.LlvmMingw.Sdk/WidlGenerateRpcClientStub.cs
@@ -13,12 +13,28 @@ namespace Sunburst.LlvmMingw.Sdk
         public ITaskItem[] GeneratedSources { get; set; }
 
         public string SymbolPrefix { get; set; }
+        public string StubType { get; set; }
+
+        protected override bool ValidateParameters()
+        {
+            if (!string.IsNullOrEmpty(StubType) && !(StubType == "Os" || StubType == "Oi" || StubType == "Oif"))
+            {
+                Log.LogError("Invailid StubType (expected \"Os\", \"Oi\", or \"Oif\")");
+                return false;
+            }
+
+            return base.ValidateParameters();
+        }
 
         protected override void GenerateCommandLineCommandsCore(CommandLineBuilder builder)
         {
             builder.AppendSwitch("-c");
             builder.AppendSwitchIfNotNull("-o ", OutputFilePath);
             builder.AppendSwitchIfNotNull("--prefix-client=", SymbolPrefix);
+
+            if (StubType == "Os") builder.AppendSwitch("-Os");
+            else if (StubType == "Oi") builder.AppendSwitch("-Oi");
+            else if (StubType == "Oif") builder.AppendSwitch("-Oif");
         }
 
         protected override void OnExecuteSuccess()
